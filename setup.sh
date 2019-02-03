7673ce0798c6751a26944a3d714e84921c6ed354
Carlos Zambrano <czam01@gmail.com>
2019-02-03 15:10:52 -0500

    Create setup.sh
    add setup to configure airflow

---
+++ setup.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+# Points to the parent folder of the setup script
+AIRFLOW_HOME="$(pwd)"
+
+cat <<EOF > $AIRFLOW_HOME/.env
+PIPENV_VENV_IN_PROJECT=True
+AIRFLOW_HOME=$AIRFLOW_HOME
+PAMPA_ETL_ENV=prd
+CW_SRC_ENV=prd
+CW_DEST_ENV=prd
+MONGO_ENV=prd
+DELIVER_DB=prd_delivery
+SEGMENT_ENV=prd
+REQUESTS_PER_SECOND=30
+EVENTS_PER_REQUEST=50
+PAMPA_ENV=prd
+EOF
+
+cat <<EOF > $AIRFLOW_HOME/airflow.cfg
+
+[core]
+airflow_home = $AIRFLOW_HOME
+dags_folder = $AIRFLOW_HOME/dags
+base_log_folder = $AIRFLOW_HOME/logs
+
+#S3 Connection Logs
+task_log_reader = s3.task
+logging_config_class = log_config.LOGGING_CONFIG
+remote_log_conn_id = s3_default
+
+remote_logging = True
+encrypt_s3_logs = False
+
+#Logging Level
+logging_level = INFO
+fab_logging_level = WARN
+
+# Log format
+log_format = [%%(asctime)s] {%%(filename)s:%%(lineno)d} %%(levelname)s - %%(message)s
+simple_log_format = %%(asctime)s %%(levelname)s - %%(message)s
+
+# Log filename format
+log_filename_template = {{ ti.dag_id }}/{{ ti.task_id }}/{{ ts }}/{{ try_number }}.log
+log_processor_filename_template = {{ filename }}.log
+
+# Hostname by providing a path to a callable, which will resolve the hostname
+hostname_callable = socket:getfqdn
+
+# Default timezone in case supplied date times are naive
+# can be utc (default), system, or any IANA timezone string (e.g. Europe/Amsterdam)
+default_timezone = utc
+
+executor = CeleryExecutor
+
+sql_alchemy_conn = postgresql+psycopg2://$RDS_DB_USER:$RDS_DB_PASS@$RDS_DB_ENDPOINT:$RDS_DB_PORT/$RDS_DB_NAME
+sql_alchemy_pool_size = 5
+sql_alchemy_pool_recycle = 3600
+parallelism = 32
+dag_concurrency = 16
+dags_are_paused_at_creation = True
+non_pooled_task_slot_count = 128
+max_active_runs_per_dag = 16
+load_examples = False
+plugins_folder = $AIRFLOW_HOME/plugins
+fernet_key = QfQ90TaCFkE9fSLJnoikJCRxj4cnqFX69D1_Lbnhinw=
+donot_pickle = False
+dagbag_import_timeout = 30
+task_runner = BashTaskRunner
+default_impersonation =
+security =
+unit_test_mode = False
+enable_xcom_pickling = True
+killed_task_cleanup_time = 60
+
+[cli]
+api_client = airflow.api.client.local_client
+endpoint_url = http://localhost:8080
+
+[api]
+auth_backend = airflow.api.auth.backend.default
+
+[operators]
+default_owner = Airflow
+default_cpus = 1
+default_ram = 512
+default_disk = 512
+default_gpus = 0
+
+[webserver]
+base_url = http://localhost:8080
+web_server_host = 0.0.0.0
+web_server_port = 8080
+web_server_ssl_cert =
+web_server_ssl_key =
+web_server_worker_timeout = 120
+worker_refresh_batch_size = 1
+worker_refresh_interval = 30
+secret_key = temporary_key
+workers = 4
+worker_class = sync
+access_logfile = -
+error_logfile = -
+expose_config = False
+owner_mode = user
+dag_default_view = tree
+dag_orientation = LR
+demo_mode = False
+log_fetch_timeout_sec = 5
+hide_paused_dags_by_default = False
+page_size = 100
+
+#Autentication
+authenticate = True
+auth_backend = airflow.contrib.auth.backends.password_auth
+# Filter the list of dags by owner name (requires authentication to be enabled)
+filter_by_owner = False
+
+[email]
+email_backend = airflow.utils.email.send_email_smtp
+
+[smtp]
+smtp_host = $SMTP_HOST
+smtp_starttls = True
+smtp_ssl = False
+smtp_user = $SMTP_USER
+smtp_password = $SMTP_PASS
+smtp_port = $SMTP_PORT
+smtp_mail_from = email@gmail.com
+
+[celery]
+celery_app_name = airflow.executors.celery_executor
+celeryd_concurrency = 16
+worker_log_server_port = 8793
+broker_url = redis://$REDIS_ENDPOINT:$REDIS_PORT/$REDIS_DB_BROKER
+celery_result_backend = redis://$REDIS_ENDPOINT:$REDIS_PORT/$REDIS_DB_RESULT
+flower_host = 0.0.0.0
+flower_port = 5555
+default_queue = default
+celery_config_options = airflow.config_templates.default_celery.DEFAULT_CELERY_CONFIG
+
+[dask]
+cluster_address = 127.0.0.1:8786
+
+[scheduler]
+job_heartbeat_sec = 5
+scheduler_heartbeat_sec = 5
+run_duration = -1
+min_file_process_interval = 0
+dag_dir_list_interval = 300
+print_stats_interval = 30
+child_process_log_directory = $AIRFLOW_HOME/logs/scheduler
+scheduler_zombie_task_threshold = 300
+catchup_by_default = True
+max_tis_per_query = 0
+statsd_on = False
+statsd_host = localhost
+statsd_port = 8125
+statsd_prefix = airflow
+max_threads = 2
+authenticate = False
+
+[ldap]
+uri =
+user_filter = objectClass=*
+user_name_attr = uid
+group_member_attr = memberOf
+superuser_filter =
+data_profiler_filter =
+bind_user = cn=Manager,dc=example,dc=com
+bind_password = insecure
+basedn = dc=example,dc=com
+cacert = /etc/ca/ldap_ca.crt
+search_scope = LEVEL
+
+[mesos]
+master = localhost:5050
+framework_name = Airflow
+task_cpu = 1
+task_memory = 256
+checkpoint = False
+authenticate = False
+
+[kerberos]
+ccache = /tmp/airflow_krb5_ccache
+principal = airflow
+reinit_frequency = 3600
+kinit_path = kinit
+keytab = airflow.keytab
+
+[github_enterprise]
+api_rev = v3
+
+[admin]
+hide_sensitive_variable_fields = True
+
+EOF
